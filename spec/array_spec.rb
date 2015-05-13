9442721ac14c9d0007c3c50c0c8b8527ce1e2dd3
rickstewart <rick.stewart@ns.sympatico.ca>
2015-05-13 18:27:55 -0300

    Rspec

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -2,22 +2,47 @@ require 'rspec'
 require 'spec_helper'
 
 describe 'class Array' do
-  before :all do
-    @an_array = [1,2,3,4,5,6]
-    @results = []
-  end
   describe "#my_each" do
-    it 'should call the given block once for each element in self, passing that element as a parameter' do
-      expect(@an_array.my_each do |element| @results << (element * 2)
-             @results
-             end).to eq([2, 4, 6, 8, 10, 12])
+    it 'calls block once for each element in self (an array), passing that element to the block as a parameter' do
+      accumulator = Array.new
+      [1, 2, 3, 4, 5, 6].my_each { |element| accumulator << (element * 2) }
+      expect(accumulator).to eq([2, 4, 6, 8, 10, 12])
     end
-    # describe "#my_each_with_index" do
-    #   it 'should sort an array of positive integers' do
-    #     expect(@bubble_sort.bubble_sort([7, 3, 4, 5, 9, 1, 2, 0])).to eq([0, 1, 2, 3, 4, 5, 7, 9])
-    #   end
-    #
-    #   true.should == false
-    # end
+  end
+
+  describe "#my_each" do
+    it 'if no block is given an Enumerator of the array is returned' do
+      accumulator = Array.new
+      enum = [1, 2, 3, 4, 5, 6].my_each
+      accumulator << enum.next
+      accumulator << enum.next
+      accumulator << enum.next
+      accumulator << enum.next
+      accumulator << enum.next
+      accumulator << enum.next
+      expect(accumulator).to eq([1, 2, 3, 4, 5, 6])
+    end
+  end
+
+  describe "#my_each_with_index" do
+    it 'calls block once for each element in self (an array), passing that element and its index to the block as parameters' do
+      accumulator = Array.new
+      [1, 2, 3, 4, 5, 6].my_each_with_index { |element, index| accumulator[index] = (element * 2) }
+      expect(accumulator).to eq([2, 4, 6, 8, 10, 12])
+    end
+  end
+
+  describe "#my_each_with_index" do
+    it 'if no block is given an Enumerator of the array is returned' do
+      accumulator = Array.new
+      enum = [1, 2, 3, 4, 5, 6].my_each
+      accumulator << enum.next
+      accumulator << enum.next
+      accumulator << enum.next
+      accumulator << enum.next
+      accumulator << enum.next
+      accumulator << enum.next
+      expect(accumulator).to eq([1, 2, 3, 4, 5, 6])
+    end
   end
 end
