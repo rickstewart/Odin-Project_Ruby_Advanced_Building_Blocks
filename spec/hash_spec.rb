4968422da0d48eeb77fe4f9506925e938977e0dc
rstewart <rick.stewart@ns.sympatico.ca>
2015-05-14 15:58:04 -0300

    May 14

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -1,9 +1,25 @@
 require 'rspec'
+require 'spec_helper'
 
-describe 'My behaviour' do
+describe 'class Hash' do
+  describe "#my_each" do
+    it 'calls block once for each key/value pair in self (a Hash), passing that pair to the block as a parameter' do
+      accumulator = Hash.new
+      {one: 1,two: 2, three: 3,four: 4}.my_each { |key, value| accumulator[key] = (value * 2) }
+      expect(accumulator).to eq({one: 2,two: 4, three: 6,four: 8})
+    end
+  end
 
-  it 'should do something' do
-
-    true.should == false
+  describe "#my_each" do
+    it 'if no block is given an Enumerator of the Hash is returned' do
+      accumulator = Array.new
+      enum = {one: 1,two: 2, three: 3,four: 4}.my_each
+      accumulator << enum.next
+      accumulator << enum.next
+      accumulator << enum.next
+      accumulator << enum.next
+      accumulator = accumulator.to_h
+      expect(accumulator).to eq({one: 1,two: 2, three: 3,four: 4})
+    end
   end
 end
